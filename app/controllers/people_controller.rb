b134322d6020d0be51d71ef36b62c0b03d5f1311
Brian Michel <brian.michel@gmail.com>
2010-04-09 11:26:23 -0400

    Fleshed out working people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,23 +1,41 @@
 class PeopleController < ApplicationController
   def list
+    @people = Person.find(:all)
   end
   
   def show
+    @person = Person.find(params[:id])
   end
   
   def new
+    @person = Person.new
   end
   
   def create
+    @person = Person.new(params[:person])
+    if @person.save
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
   end
 
   def update
+    @person = Person.find(params[:id])
+    if @person.update_attributes(params[:person])
+      redirect_to :action => 'show', :id => @person
+    else
+      render :action => 'edit'
+    end
   end
 
   def edit
+    @person = Person.find(params[:id])
   end
 
   def delete
+    Person.find(params[:id]).destroy
+    redirect_to :action => 'list'
   end
 
 end
