8ee2d3e0678693c7d6d3edd1eca7487c2ed511b8
Brian Michel <brian.michel@gmail.com>
2010-04-09 13:38:55 -0400

    Added various URL constructors to the application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def full_name(firstname, lastname)
+    "#{firstname} #{lastname}"
+  end
+  
+  def twitter_url(twittername)
+    "http://www.twitter.com/#{twittername}"
+  end
+  
+  def tumblr_url(tumblrname)
+    "http://#{tumblrname}.tumblr.com"
+  end
+  
+  def myspace_url(myspacename)
+    "http://www.myspace.com/#{myspacename}"
+  end
+  
+  def linkedin_url(linkedinname)
+    "http://www.linkedin.com/in/#{linkedinname}"
+  end
 end
